371031ada8fa9f5272f3d1fafb5b07f65e248e26
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-03-01 22:16:03 +0545

    Add axios post to server

--- src/screens/DriverSignup/index.js
+++ src/screens/DriverSignup/index.js
@@ -1,24 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
+// package
+import axios from "axios";
+
+// styles
 import * as D from "./styles.js";
 
 const DriverSignup = () => {
   const [name, setName] = useState("");
   const [contact, setContact] = useState();
   const [email, setEmail] = useState("");
-  const [driverId, setDriverId] = useState("");
+  const [driver_id, setDriverId] = useState("");
   const [password, setPassword] = useState("");
   const [file, setFile] = useState();
 
+  const inputRef = useRef();
+
+  function resetForm() {
+    setName("");
+    setContact();
+    setContact("");
+    setEmail("");
+    setDriverId("");
+    setPassword("");
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(name, contact, email, driverId, password);
+    axios
+      .post(`/driver_signup`, {
+        // data to be sent
+        name,
+        email,
+        password,
+        driver_id,
+        contact
+      })
+      .then(response => {
+        console.log(response.data);
+        resetForm();
+        alert("Thank You for registering as an Ambulance Driver!");
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   return (
     <D.Container>
       <D.Header>Lets get you registered for Lifeline Driver App!</D.Header>
-      <D.FormContainer onSubmit={e => handleSubmit(e)}>
+      <D.FormContainer ref={inputRef} onSubmit={e => handleSubmit(e)}>
         <D.FormRow>
           <D.InputLabel>Name</D.InputLabel>
           <D.FormInput
@@ -53,7 +84,7 @@ const DriverSignup = () => {
           <D.InputLabel>Driver Id</D.InputLabel>
           <D.FormInput
             type="text"
-            value={driverId}
+            value={driver_id}
             placeholder="Enter your Driver id/Licence no..."
             onChange={e => setDriverId(e.target.value)}
           />
@@ -70,11 +101,7 @@ const DriverSignup = () => {
         </D.FormRow>
 
         <D.FormRow>
-          <D.FormInput
-            type="file"
-            value={file}
-            onChange={e => setFile(e.target.value)}
-          />
+          <D.FormInput type="file" onChange={e => setFile(e.target.files[0])} />
           <button>Upload</button>
         </D.FormRow>
 
