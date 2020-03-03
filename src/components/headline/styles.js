9ffcc1d0c1b142aeff5d105d39c532381c793f04
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-03-03 21:46:26 +0545

    add back button

--- src/components/headline/styles.js
+++ src/components/headline/styles.js
@@ -11,16 +11,43 @@ export const Container = styled.div`
 `;
 
 export const HeaderContainer = styled.div`
+  display: flex;
   flex-direction: row;
 `;
 
 export const Logo = styled.img`
   width: 85px;
   height: 85px;
-  margin-right: 1em;
+  margin-left: 1.8em;
+  margin-right: 1.4em;
 `;
 
 export const HeaderText = styled.p`
   color: white;
   font-size: 1.6em;
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const List = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: auto;
+  margin-right: 2em;
+  justify-content: space-between;
+`;
+
+export const Link = styled.button`
+  border: none;
+  border-style: none;
+  background-color: transparent;
+  color: rgba(255, 255, 255, 0.6);
+  font-size: 1.2em;
+  cursor: pointer;
+  transition: 0.6s;
+  :hover {
+    color: rgba(255, 255, 255, 1);
+    font-size: 1.4em;
+  }
 `;
